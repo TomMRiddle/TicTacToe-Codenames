30a61ca2a77668932aaaf9b293a5fb0a3a0011b4
Victor Ek <victor.ek@gmail.com>
2024-11-01 12:38:26 +0100

    trying to fix idea project

--- Main.java
+++ Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Board board = new TicTacToeBoard();
         System.out.println(board);
+        board.getCellById(5).setContent("O");
+        System.out.println(board);
+
     }
 }
