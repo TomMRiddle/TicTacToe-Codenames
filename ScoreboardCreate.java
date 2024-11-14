cf16ee19fdaed5ddfadcd19570e2ea5ce45b0e5e
Moa <m.l.elofsson@gmail.com>
2024-11-14 15:40:44 +0100

    Createmetod för scoreboard

---
+++ ScoreboardCreate.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class ScoreboardCreate {
+    public List<Score> newScoreboard(){
+        List<Score> highscores = new ArrayList<>();
+        return highscores;
+    }
+}
